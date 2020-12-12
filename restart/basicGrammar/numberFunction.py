189a2667eb49806f63e60b52e60a9f2250c04d37
zave <zave7@naver.com>
2020-12-12 23:14:50 +0900

    restart python (basic grammar)

---
+++ restart/basicGrammar/numberFunction.py
@@ -0,0 +1,14 @@
+# 숫자 처리 관련 함수
+
+print( abs(-5))
+print( pow(4, 2))
+print( 4**2)
+print( max(1, 4))
+print( min(4, 2))
+print( round( 3.12)) # round 반올림
+print( round( 4.99))
+
+from math import *
+print( floor(4.99)) # 내림
+print( ceil(3.14)) # 올림
+print( sqrt(16))
